61855b450054d288385085339b83bdd6f96b3fa0
benjaminkeithrose-svg <benjamin.keith.rose@gmail.com>
2026-09-03 11:37:26 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'beltcall-v1';
+const CACHE = 'beltcall-v2';
 const ASSETS = [
   './', './index.html', './app.js', './manifest.webmanifest',
   'https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js'
